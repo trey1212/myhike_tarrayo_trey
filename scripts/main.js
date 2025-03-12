7a18ebc42e2efc7e8934d8a2e089a5980da1a0cb
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2025-03-11 19:55:14 -0700

    demo 10

--- scripts/main.js
+++ scripts/main.js
@@ -72,6 +72,7 @@ function displayCardsDynamically(collection) {
         .then(allHikes=> {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
             allHikes.forEach(doc => { //iterate thru each doc
+                var docID = doc.id;               // get the unique ID of the document
                 var title = doc.data().name;       // get value of the "name" key
                 var details = doc.data().details;  // get value of the "details" key
 								var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
@@ -83,6 +84,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID; //pass the docID as a query string
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
